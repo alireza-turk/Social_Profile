5cbeb3460dbd4aa313893f60c9aff1937ebffb8f
AlirezaAhmadyan <ahmadyalireza111@gmail.com>
2024-08-22 08:04:50 +0330

    design card-desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,16 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'primary': 'hsl(75, 94%, 57%)',
+        'primary': '#00d801',
+        'primary-dark': 'hsl(75, 94%, 57%)',
         'g-700':'hsl(0, 0%, 20%)',
         'g-800':'hsl(0, 0%, 12%)',
         'g-900':'hsl(0, 0%, 8%)',
       },
       fontFamily:{
-        'inter-regular':'inter-400',
-        'inter-bold':'inter-600',
-        'inter-semibold':'inter-700'
+        'inter_regular':'inter-400',
+        'inter_bold':'inter-700',
+        'inter_semibold':'inter-600'
       }
     },
   },
